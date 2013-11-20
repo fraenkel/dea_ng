c0aca1c0377bcafd1774440502b2440a4e3323d1
fraenkel <fraenkel@us.ibm.com>
2013-11-20 15:18:03 -0500

    loggregator tests

---
+++ go/src/dea/loggregator/loggregator_test.go
@@ -0,0 +1,100 @@
+package loggregator_test
+
+import (
+	. "dea/loggregator"
+	temitter "dea/testhelpers/emitter"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Loggregator", func() {
+	var emitter temitter.MockEmitter
+	var staging_emitter temitter.MockEmitter
+
+	BeforeEach(func() {
+		emitter = temitter.MockEmitter{}
+		SetEmitter(&emitter)
+		staging_emitter = temitter.MockEmitter{}
+		SetStagingEmitter(&staging_emitter)
+	})
+
+	Describe("dea emitter", func() {
+		Describe("emit", func() {
+			It("emits to the loggregator", func() {
+				Emit("my_app_id", "important log message")
+				Expect(emitter.Messages).To(HaveLen(1))
+				Expect(emitter.Messages["my_app_id"][0]).To(Equal("important log message"))
+			})
+
+			It("does not emit if there is no loggregator", func() {
+				SetEmitter(nil)
+				Emit("my_app_id", "important log message")
+				Expect(emitter.Messages).To(HaveLen(0))
+			})
+
+			It("does not emit if there is no application id", func() {
+				Emit("", "important log message")
+				Expect(emitter.Messages).To(HaveLen(0))
+			})
+		})
+
+		Describe("emiterror", func() {
+			It("emits to the loggregator", func() {
+				EmitError("my_app_id", "important log message")
+				Expect(emitter.ErrorMessages).To(HaveLen(1))
+				Expect(emitter.ErrorMessages["my_app_id"][0]).To(Equal("important log message"))
+			})
+
+			It("does not emit if there is no loggregator", func() {
+				SetEmitter(nil)
+				EmitError("my_app_id", "important log message")
+				Expect(emitter.ErrorMessages).To(HaveLen(0))
+			})
+
+			It("does not emit if there is no application id", func() {
+				EmitError("", "important log message")
+				Expect(emitter.ErrorMessages).To(HaveLen(0))
+			})
+		})
+	})
+
+	Describe("staging emitter", func() {
+		Describe("emit", func() {
+			It("emits to the loggregator", func() {
+				StagingEmit("my_app_id", "important log message")
+				Expect(staging_emitter.Messages).To(HaveLen(1))
+				Expect(staging_emitter.Messages["my_app_id"][0]).To(Equal("important log message"))
+			})
+
+			It("does not emit if there is no loggregator", func() {
+				SetStagingEmitter(nil)
+				StagingEmit("my_app_id", "important log message")
+				Expect(staging_emitter.Messages).To(HaveLen(0))
+			})
+
+			It("does not emit if there is no application id", func() {
+				StagingEmit("", "important log message")
+				Expect(staging_emitter.Messages).To(HaveLen(0))
+			})
+		})
+
+		Describe("emiterror", func() {
+			It("emits to the loggregator", func() {
+				StagingEmitError("my_app_id", "important log message")
+				Expect(staging_emitter.ErrorMessages).To(HaveLen(1))
+				Expect(staging_emitter.ErrorMessages["my_app_id"][0]).To(Equal("important log message"))
+			})
+
+			It("does not emit if there is no loggregator", func() {
+				SetStagingEmitter(nil)
+				StagingEmitError("my_app_id", "important log message")
+				Expect(staging_emitter.ErrorMessages).To(HaveLen(0))
+			})
+
+			It("does not emit if there is no application id", func() {
+				StagingEmitError("", "important log message")
+				Expect(staging_emitter.ErrorMessages).To(HaveLen(0))
+			})
+		})
+	})
+})
